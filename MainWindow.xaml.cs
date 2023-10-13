07e496a25e6fc16150f597c2d7f3d691e8ec12b2
Chen Buyi <chenbuyi2019@outlook.com>
2023-10-13 16:59:23 +0800

    wpf release

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -11,7 +11,9 @@ using System.Diagnostics;
 using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Interop;
 using System.Windows.Media;
+using System.Windows.Media.Imaging;
 using System.Windows.Media.Media3D;
 
 namespace BuyiTools
